2c09b4d22161df0c7504085bd9b8dfd3fda5135c
Kostyantyn Babiy <Kostyantyn_Babiy@epam.com>
2019-08-25 01:37:12 +0200

    added more model details

---
+++ BddStyle.NUnit.Test/Examples/Model/given_setup1/and_sub_setup1/Context.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace BddStyle.NUnit.Test.Examples.Model.given_setup1.and_sub_setup1
+{
+    public abstract class Context : given_setup1.Context
+    {
+        protected override void Arrange()
+        {
+            base.Arrange();
+        }
+    }
+}
---
+++ BddStyle.NUnit.Test/Examples/Model/given_setup1/and_sub_setup1/when_case42.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+namespace BddStyle.NUnit.Test.Examples.Model.given_setup1.and_sub_setup1
+{
+    public class when_case42 : Context
+    {
+        [Test]
+        public void then_assertion()
+        { }
+    }
+}
